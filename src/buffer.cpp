15ef49eecaa24a57b1e25b7dadd43b50c1492ca9
kmui2 <kmui2@wisc.edu>
2017-10-11 17:28:57 -0500

    implementing deconstructor

--- src/buffer.cpp
+++ src/buffer.cpp
@@ -14,78 +14,83 @@
 #include "exceptions/bad_buffer_exception.h"
 #include "exceptions/hash_not_found_exception.h"
 
-namespace badgerdb { 
+namespace badgerdb
+{
 
 BufMgr::BufMgr(std::uint32_t bufs)
-	: numBufs(bufs) {
+	: numBufs(bufs)
+{
 	bufDescTable = new BufDesc[bufs];
 
-  for (FrameId i = 0; i < bufs; i++) 
-  {
-  	bufDescTable[i].frameNo = i;
-  	bufDescTable[i].valid = false;
-  }
+	for (FrameId i = 0; i < bufs; i++)
+	{
+		bufDescTable[i].frameNo = i;
+		bufDescTable[i].valid = false;
+	}
 
-  bufPool = new Page[bufs];
+	bufPool = new Page[bufs];
 
-	int htsize = ((((int) (bufs * 1.2))*2)/2)+1;
-  hashTable = new BufHashTbl (htsize);  // allocate the buffer hash table
+	int htsize = ((((int)(bufs * 1.2)) * 2) / 2) + 1;
+	hashTable = new BufHashTbl(htsize); // allocate the buffer hash table
 
-  clockHand = bufs - 1;
+	clockHand = bufs - 1;
 }
 
-
-BufMgr::~BufMgr() {
+BufMgr::~BufMgr()
+{
+	for (FrameId i = 0; i < numBufs; i++)
+	{
+		if (bufDescTable[i].dirty) {
+			BufMgr::flushFile(bufDescTable[i].file);
+		}
+	}
+	delete bufDescTable;
+	delete bufPool;
 }
 
 void BufMgr::advanceClock()
 {
-
 }
 
-void BufMgr::allocBuf(FrameId & frame) 
+void BufMgr::allocBuf(FrameId &frame)
 {
 }
 
-	
-void BufMgr::readPage(File* file, const PageId pageNo, Page*& page)
+void BufMgr::readPage(File *file, const PageId pageNo, Page *&page)
 {
 }
 
-
-void BufMgr::unPinPage(File* file, const PageId pageNo, const bool dirty) 
+void BufMgr::unPinPage(File *file, const PageId pageNo, const bool dirty)
 {
 }
 
-void BufMgr::flushFile(const File* file) 
+void BufMgr::flushFile(const File *file)
 {
 }
 
-void BufMgr::allocPage(File* file, PageId &pageNo, Page*& page) 
+void BufMgr::allocPage(File *file, PageId &pageNo, Page *&page)
 {
 }
 
-void BufMgr::disposePage(File* file, const PageId PageNo)
+void BufMgr::disposePage(File *file, const PageId PageNo)
 {
-    
 }
 
-void BufMgr::printSelf(void) 
+void BufMgr::printSelf(void)
 {
-  BufDesc* tmpbuf;
+	BufDesc *tmpbuf;
 	int validFrames = 0;
-  
-  for (std::uint32_t i = 0; i < numBufs; i++)
+
+	for (std::uint32_t i = 0; i < numBufs; i++)
 	{
-  	tmpbuf = &(bufDescTable[i]);
+		tmpbuf = &(bufDescTable[i]);
 		std::cout << "FrameNo:" << i << " ";
 		tmpbuf->Print();
 
-  	if (tmpbuf->valid == true)
-    	validFrames++;
-  }
+		if (tmpbuf->valid == true)
+			validFrames++;
+	}
 
 	std::cout << "Total Number of Valid Frames:" << validFrames << "\n";
 }
-
 }
